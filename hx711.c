591ddb3a4a85d70f671c7420204e2ab2d59316d2
NonXtreme <30690018+NonXtreme@users.noreply.github.com>
2018-12-17 22:56:14 +0700

    Add more functions
    -Add average read
    -Add offset and tare function
    -Add get value function that return the reading with offset

--- hx711.c
+++ hx711.c
@@ -17,7 +17,8 @@ int HX711_Read(HX711* H) {
 	for (int i = 0; i < 24; i++) {
 		HAL_GPIO_WritePin(H->PD_SCK_PinType, H->PD_SCK_PinNumber, GPIO_PIN_SET);
 		count = count << 1;
-		HAL_GPIO_WritePin(H->PD_SCK_PinType, H->PD_SCK_PinNumber, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(H->PD_SCK_PinType, H->PD_SCK_PinNumber,
+				GPIO_PIN_RESET);
 		if (HAL_GPIO_ReadPin(H->DOUT_PinType, H->DOUT_PinNumber)) {
 			if (i == 0) {
 				isNegative = 1;
@@ -32,7 +33,28 @@ int HX711_Read(HX711* H) {
 	HAL_GPIO_WritePin(H->PD_SCK_PinType, H->PD_SCK_PinNumber, GPIO_PIN_RESET);
 	for (int i = 0; i < H->mode; i++) {
 		HAL_GPIO_WritePin(H->PD_SCK_PinType, H->PD_SCK_PinNumber, GPIO_PIN_SET);
-		HAL_GPIO_WritePin(H->PD_SCK_PinType, H->PD_SCK_PinNumber, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(H->PD_SCK_PinType, H->PD_SCK_PinNumber,
+				GPIO_PIN_RESET);
 	}
 	return count;
+}
+
+int HX711_AvgRead(HX711* H, int times) {
+	int sum = 0;
+	for (int i = 0; i < times; i++) {
+		sum += HX711_Read(H);
+	}
+	return sum / times;
+}
+
+void HX711_Tare(HX711* H, int times) {
+	H->offset = HX711_AvgRead(H, times);
+}
+
+int HX711_GetValue(HX711* H) {
+	return HX711_Read(H) - (H->offset);
+}
+
+int HX711_GetAvgValue(HX711* H, int times) {
+	return HX711_AvgRead(H, times) - (H->offset);
 }
--- hx711.h
+++ hx711.h
@@ -15,11 +15,16 @@ typedef struct {
 	uint16_t PD_SCK_PinNumber;		//GPIO_Pin
 	GPIO_TypeDef* DOUT_PinType;		//GPIOx
 	uint16_t DOUT_PinNumber;		//GPIO_Pin
-	int mode;		// 0 Input channel A ,gain=128
-					// 1 Input channel B ,gain=32
-					// 2 Input channel A ,gain=64
-}HX711;
+	int offset;
+	int mode;		// 0 Input channel A, gain=128
+					// 1 Input channel B, gain=32
+					// 2 Input channel A, gain=64
+} HX711;
 
 int HX711_Read(HX711* H);
+int HX711_AvgRead(HX711* H, int times);
+void HX711_Tare(HX711* H, int times);
+int HX711_GetValue(HX711* H);
+int HX711_GetAvgValue(HX711* H, int times);
 
 #endif /* HX711_H_ */
